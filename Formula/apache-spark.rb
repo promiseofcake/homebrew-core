42128128dfc7e5c2efedb2c1962abde1e09e7330
Lucas Kacher <lucas@vsco.co>
2018-03-01 16:28:56 -0500

    apache-spark 2.3.0

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,8 +1,8 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz"
-  version "2.2.1"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz"
+  version "2.3.0"
   sha256 "79fb8285546670923a66082324bf56e99a7201476a52dea908804ddfa04f16c8"
   head "https://github.com/apache/spark.git"
 
